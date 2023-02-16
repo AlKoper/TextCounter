dddc0972cb68a88b61629433325209bd5244b29e
AlKoper <AlexanderKoperman@gmail.com>
2023-02-16 22:33:12 +0300

    feat: Обновил GUI и добавил возможность рассчетов .docx файлов
    Причесал GUIк нормальному виду, всё выглядит красви и гармотично
    Создал функцию docx_to_txt, которая сохраяет тексты из .docx в .txt форматах в соответствующих файлах. Далее эти тексты обрабатывает основная функция, которая рассчитывает ко-во символов и слов, и выводит общее значение
    
    Обновил readme.md и requirements.txt

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from docx import Document
 
 # создам функцию, которая открывает FileLinks.txt и берет адрес, где будут хранится исходные файлы
 def open_filelinks():
@@ -22,14 +22,24 @@ def text_counter():
                 counter_text_words = counter_text_words + amount_words
             clear_line = line.translate({ord(i): None for i in "\n,' ',"})    # чистим каждую линию от символов (' ', '\n'), используем метод .translate
             counter_text_chars = counter_text_chars + len(clear_line)    # прибавляем количесвто сиволов каждой строки к счетчику
-            # print(counter_text_words)
         counter_words = counter_words + counter_text_words
         counter_chars = counter_chars + counter_text_chars    # прибавлеем количесвто символов в файле к общему счетчику
-    print(f'Общее количество символов нескольких файлов составляет: {counter_chars}, слов: {counter_words}')
+    # print(f'Общее количество символов нескольких файлов составляет: {counter_chars}, слов: {counter_words}')
     return counter_chars, counter_words
 
 
-if __name__ == '__main__':
-    text_counter()
+# Создам функцию, которая будет открывать .docx файлы и сохранять их тексты в формате .txt для дальнейшей обработки
+def docx_to_txt():
+    for text_file in (Path(open_filelinks()).glob('*.docx')):
+        doc = Document(text_file)
+        text = []
+        filename = open_filelinks() + text_file.name[0:-5:1] + '.txt'
+        for paragraph in doc.paragraphs:
+            text.append(paragraph.text)
+        file = '\n'.join(text)
+        save_file = open(filename, 'w')
+        save_file.write(file)
+        save_file.close()
+
 
 
--- view.py
+++ view.py
@@ -23,7 +23,13 @@ def center(win):
 
 # Создадим обработчик для кнопки button_start:
 def button_start_handler(event):
-    amount_chars, amount_words = model.text_counter()   # вызовим функцию расчета ко-ва слов и символов
+    format_files = ''    # создадим счетчик, чтобы отслеживать формат текстов
+    format_files = combo_format.get()    # рассмотрим, какой формат у файлов
+    if format_files == 'docx':
+        model.docx_to_txt()    # запустим функцию для сохранения текстов из  .docx в .txt
+        amount_chars, amount_words = model.text_counter()  # вызовим функцию расчета ко-ва слов и символов
+    else:
+        amount_chars, amount_words = model.text_counter()  # вызовим функцию расчета ко-ва слов и символов
     entry_chars.delete(0, END)    # зачистим поле с ко-вом символов
     entry_chars.insert(0, amount_chars)    # вставим в поле ко-во символов
     entry_words.delete(0, END)    # зачистим поле с ко-вом слов
@@ -31,49 +37,52 @@ def button_start_handler(event):
     order_price = float(combo_price.get())    # возбмем цену заказа из соотвествующего окна
     order_cost = order_price * (amount_chars/1000)    # рассчитаем стоимость заказа из вводных данных
     text_counter.delete(1.0, END)    # зачистим текстовое поле
-    conclusion = 'Объем всего материала составляет '+str(amount_words)+' слов, или '+str(amount_chars)+' з.б.п. Отсюду получаем стоимость заказа:'+'\n'+'$'+str(order_price)+' * '+str(amount_chars/1000)+' = $'+str(round(order_cost, 2))
+    conclusion = 'Объем всего материала составляет '+str(amount_words)+' слов, или '+str(amount_chars)+' з.б.п. Отсюда получаем стоимость заказа:'+'\n'+'$'+str(order_price)+' * '+str(amount_chars/1000)+' = $'+str(round(order_cost, 2))
     text_counter.insert(1.0, conclusion)    # вставим ответ в текстовое поле со всеми данными
 
 
 # Создадим функцию для создания главного окна программы
 def main_window():
-    global entry_chars, entry_words, combo_price, text_counter
+    global entry_chars, entry_words, combo_price, combo_format, text_counter
     # Создадим главное окно программы
     root = Tk()
-    root.geometry('400x340')
+    root.geometry('310x308')
     root.title('Text Counter')
     root.config(bg='light blue')
     center(root)
     # Создам виджеты для размещения в окне (подписи, текстовое поле, выпадающие меню и кнопку)
-    label_format = Label(root, text='Формат:', font='Calibri 15', background='light blue', width=10)
+    label_format = Label(root, text='Формат:', font='Calibri 15', background='light blue', width=12)
     label_price = Label(root, text='Цена:', font='Calibri 15', background='light blue', width=10)
-    combo_format = Combobox(root)
-    combo_price = Combobox(root)
+    combo_format = Combobox(root, width=10)
+    combo_price = Combobox(root, width=10)
     combo_format_values = ['docx', 'txt']
     combo_price_values = ['3.5', '2.5']
     combo_format['values'] = combo_format_values
     combo_price['values'] = combo_price_values
     combo_format.current(0)
     combo_price.current(0)
-    label_amount = Label(root, text='Количество', font='Calibri 17', background='light blue', width=20)
-    label_words = Label(root, text='Слова:', font='Calibri 15', background='light blue', width=10)
-    label_chars = Label(root, text='Символы:', font='Calibri 15', background='light blue', width=10)
-    entry_words = Entry(root, width=10)
-    entry_chars = Entry(root, width=10)
-    text_counter = Text(root, width=34, height=4)
-    button_start = Button(root, text='Поехали', width=30)
+    label_words = Label(root, text='Слова:', font='Calibri 15', background='light blue', width=11)
+    label_chars = Label(root, text='Символы:', font='Calibri 15', background='light blue', width=13)
+    entry_words = Entry(root, width=11)
+    entry_chars = Entry(root, width=11)
+    text_counter = Text(root, width=33, height=4)
+    button_start = Button(root, text='Поехали', font='Calibri 17', width=16)
     #размещу виджеты в окне
-    label_format.grid(row=0, column=0)
-    combo_format.grid(row=0, column=1)
-    label_price.grid(row=1, column=0)
-    combo_price.grid(row=1, column=1)
-    label_amount.grid(row=2, columnspan=2)
-    label_words.grid(row=3, column=0)
-    entry_words.grid(row=3, column=1)
-    label_chars.grid(row=4, column=0)
-    entry_chars.grid(row=4, column=1)
-    text_counter.grid(row=5, columnspan=2)
-    button_start.grid(row=6, columnspan=2, pady=10, padx=0)
+    line = Canvas(root, width=280, height=15, bg='light blue', highlightthickness=0)
+    line.grid(row=0, padx=0, columnspan=2)
+    label_format.grid(row=1, column=0, sticky=E, pady=0)
+    combo_format.grid(row=1, column=1, sticky=W)
+    label_price.grid(row=2, column=0, sticky=E)
+    combo_price.grid(row=2, column=1, sticky=W)
+    line = Canvas(root, width=280, height=30, bg='light blue', highlightthickness=0)
+    line.create_line(20, 15, 260, 15, fill='black', width=2)
+    line.grid(row=3, padx=0, columnspan=2)
+    label_words.grid(row=4, column=0, sticky=E)
+    entry_words.grid(row=4, column=1, sticky=W)
+    label_chars.grid(row=5, column=0, sticky=E, padx=3)
+    entry_chars.grid(row=5, column=1, sticky=W)
+    text_counter.grid(row=6, columnspan=2, padx=20, pady=10)
+    button_start.grid(row=7, columnspan=2, pady=0, padx=0)
     # привяжу обработчик к кнопке button_start
     button_start.bind('<Button-1>', button_start_handler)
 
